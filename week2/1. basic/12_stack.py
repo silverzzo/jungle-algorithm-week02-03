f5708060e4f0e86eafb35361abcca45ef3523025
silverzzo <124555750+silverzzo@users.noreply.github.com>
2026-08-29 19:39:27 +0900

    feat: solve parentheses matching problem (12_stack.py)

--- week2/1. basic/12_stack.py
+++ week2/1. basic/12_stack.py
@@ -38,14 +38,24 @@ def is_valid_parentheses(s):
     stack = []
     
     # TODO: 문자열의 각 문자를 순회
-    ## : 여는 괄호 '('면 스택에 추가
-    ## : 닫는 괄호 ')'면
-    ## 스택이 비어있으면 False 반환
-    ## 아니면 스택에서 pop
-    pass
+    for i in s:
+            ## : 여는 괄호 '('면 스택에 추가
+            if i=='(':
+                stack.append(i)
+            ## : 닫는 괄호 ')'면
+            elif i==')':
+                ## 스택이 비어있으면 False 반환
+                if stack==[]:
+                    return False
+                ## 아니면 스택에서 pop
+                else:
+                    stack.pop()
     
-    # TODO: 반복이 끝나면 스택이 비어있는지 확인
-    pass
+        # TODO: 반복이 끝나면 스택이 비어있는지 확인
+    if stack==[]:
+        return True
+    else:
+        return False
 
 # 테스트 케이스
 if __name__ == "__main__":
