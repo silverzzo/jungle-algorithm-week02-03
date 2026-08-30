ecf43418809330a1de0126540d16e2821bdd559b
silverzzo <124555750+silverzzo@users.noreply.github.com>
2026-08-30 19:33:53 +0900

    feat: solve find maximum with divide and conquer problem (11_divide_conquer.py)

--- week2/1. basic/11_divide_conquer.py
+++ week2/1. basic/11_divide_conquer.py
@@ -36,19 +36,22 @@ def find_max_divide_conquer(arr, left, right):
         최댓값
     """
     # TODO: base case - 원소가 하나면 그 값 반환
-    pass
+    if left==right:
+        return arr[left]
     
     # TODO: 중간 지점 계산
-    pass
+    mid=(left+right)//2
     
     # TODO: 왼쪽 절반의 최댓값
-    pass
+    l_max=find_max_divide_conquer(arr,left,mid)
         
     # TODO: 오른쪽 절반의 최댓값
-    pass
+    r_max=find_max_divide_conquer(arr,mid+1,right)
     
     # TODO: 둘 중 큰 값 반환
-    pass
+    if l_max>r_max:
+        return l_max
+    else: return r_max
 
 # 테스트 케이스
 if __name__ == "__main__":
