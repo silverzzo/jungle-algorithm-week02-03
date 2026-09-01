ce5df7470d58c605e295dafc47de25f574d03afb
silverzzo <124555750+silverzzo@users.noreply.github.com>
2026-09-01 13:11:44 +0900

    feat: solve number theory problem (09_number_theory.py)

--- week2/1. basic/09_number_theory.py
+++ week2/1. basic/09_number_theory.py
@@ -36,8 +36,11 @@ def gcd(a, b):
     """
     # TODO: 유클리드 호제법 구현
     # base case: b가 0이면 a 반환
-    # recursive를 이용 
-    pass
+    # recursive를 이용   
+    if b==0:
+        return a
+    else:
+        return gcd(b,a%b)
 
 def gcd_iterative(a, b):
     """
@@ -51,7 +54,11 @@ def gcd_iterative(a, b):
     """
     # TODO: 반복문으로 구현
     # b가 0이 될 때까지 반복
-    pass
+    while(b!=0):
+        temp=b
+        b=a%b
+        a=temp
+    return a
 
 def lcm(a, b):
     """
@@ -64,7 +71,7 @@ def lcm(a, b):
         최소공배수
     """
     # TODO: LCM 계산
-    pass
+    return(a*b)//gcd(a,b)
 
 def extended_gcd(a, b):
     """
@@ -81,7 +88,17 @@ def extended_gcd(a, b):
     # base case: b가 0이면 (a, 1, 0) 반환    
     # recursive case
     # 역추적하며 x, y 계산
-    pass
+    if b==0:
+        return (a,1,0)
+    
+    g,x1,y1=extended_gcd(b,a%b)
+    
+    x=y1
+    y=x1-(a//b)*y1
+    return(g,x,y)
+
+
+from math import sqrt
 
 def is_prime(n):
     """
@@ -97,7 +114,19 @@ def is_prime(n):
     # n이 2보다 작으면 False
     # 2부터 sqrt(n)까지 나누어 떨어지는지 확인    
     # 3부터 sqrt(n)까지 홀수만 확인
-    pass 
+    if n<2:
+        return False
+    elif n==2:
+        return True
+    elif n%2==0:
+        return False
+    else:
+        for i in range(3,int(sqrt(n))+1,2):
+            if n%i==0:
+                return False
+        return True
+    
+  
 
 # 테스트 케이스
 if __name__ == "__main__":
