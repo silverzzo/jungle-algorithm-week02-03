2bc1496a7285e0842250eb573ab02677d826693a
silverzzo <124555750+silverzzo@users.noreply.github.com>
2026-09-05 14:26:39 +0900

    feat: solve BFS graph traversal problem (04_bfs.py)

--- week3/1. basic/04_bfs.py
+++ week3/1. basic/04_bfs.py
@@ -43,18 +43,26 @@ def bfs(graph, start):
     Returns:
         방문 순서 리스트
     """
-    visited = []
+    visited = [start]
     
     # TODO: 큐 생성 및 시작 정점 추가
     ## 방문한 정점 집합
-    pass
-
+    queue = deque([start])
+    
     # TODO: 큐가 빌 때까지 반복
     ## 큐에서 정점 꺼내기
     ## 인접한 정점들 확인
     ## 방문하지 않은 정점이면 큐에 추가
-    pass
-    
+    while queue:
+        #current==실제로 방문하는 정점
+        current=queue.popleft()
+                
+        #i==current와 연결된 이웃 정점
+        for i in graph[current]:
+            if i not in visited:
+                queue.append(i)
+                visited.append(i)
+
     return visited
 
 # 테스트 케이스
