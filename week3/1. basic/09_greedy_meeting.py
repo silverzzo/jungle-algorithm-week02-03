9df4b18089bb5b0b311901511e1303ed3536ac42
silverzzo <124555750+silverzzo@users.noreply.github.com>
2026-09-07 13:59:32 +0900

    feat: solve greedy meeting room scheduling problem (09_greedy_meeting.py)

--- week3/1. basic/09_greedy_meeting.py
+++ week3/1. basic/09_greedy_meeting.py
@@ -33,20 +33,23 @@ def select_meetings(meetings):
         (배정된 회의 개수, 선택된 회의 리스트)
     """
     # TODO: 회의가 없으면 0 반환
-    pass
+    if not meetings:
+        return 0,[]
     
     # TODO: 종료 시간 기준으로 정렬
-    pass
     
+    meetings.sort(key=lambda x: x[1])
     selected = []
     
     # TODO: 첫 번째 회의 선택
-    pass
+    selected.append(meetings[0])
     
     # TODO: 나머지 회의들 확인
     ## 이전 회의가 끝난 후 시작하는 회의만 선택
-    pass
-    
+    for meeting in meetings[1:]:
+        if meeting[0]>=selected[-1][1]:
+            selected.append(meeting)
+        
     return len(selected), selected
 
 # 테스트 케이스
