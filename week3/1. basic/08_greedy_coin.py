3a3dc5a6327e263243626270d5fb1e1ea2bc2975
silverzzo <124555750+silverzzo@users.noreply.github.com>
2026-09-07 13:22:41 +0900

    feat: solve greedy coin change problem (08_greedy_coin.py)

--- week3/1. basic/08_greedy_coin.py
+++ week3/1. basic/08_greedy_coin.py
@@ -46,8 +46,13 @@ def make_change_greedy(change, coins):
     # TODO: 각 동전에 대해 반복
     ## 현재 동전으로 거슬러줄 수 있는 개수 계산    
     ## 개수가 0보다 크면 결과에 추가
-    pass
-    
+    for c in coins:
+        count=change//c
+
+        if count>0:
+            total_coins+=count
+            result[c]=count
+            change=change%c
     return total_coins, result
 
 # 테스트 케이스
